3f712b31cb346df820bfee63bbc1250d09f4db80
Kaiwu Li <kaiwu.li@carsales.com.au>
2018-12-07 17:32:40 +1100

    call AndroidSectionViewInjection.inject

--- app/src/main/java/au/com/kaiwu/nestfragmentdagger/main/childfragment/ChildFragment.kt
+++ app/src/main/java/au/com/kaiwu/nestfragmentdagger/main/childfragment/ChildFragment.kt
@@ -2,16 +2,18 @@ package au.com.kaiwu.nestfragmentdagger.main.childfragment
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import au.com.kaiwu.nestfragmentdagger.AndroidSectionViewInjection
 import au.com.kaiwu.nestfragmentdagger.R
 import au.com.kaiwu.nestfragmentdagger.TestData
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 import javax.inject.Named
 
-class ChildFragment : DaggerFragment() {
+class ChildFragment : /*Dagger*/Fragment() {
 
 
     companion object {
@@ -20,8 +22,17 @@ class ChildFragment : DaggerFragment() {
 
     private lateinit var viewModel: ChildViewModel
 
+    @Inject
+    @field:Named("provideCustomViewTestData")
+    lateinit var testData: TestData
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        AndroidSectionViewInjection.inject(container, this)
+        if (testData != null) {
+
+        }
         return inflater.inflate(R.layout.child_fragment, container, false)
     }
 
@@ -29,7 +40,6 @@ class ChildFragment : DaggerFragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ChildViewModel::class.java)
         // TODO: Use the ViewModel
-
 
 
     }
