7119b177c0399ec8ee0fc7137cfc9ad26d7d90e6
Kaiwu Li <kaiwu.li@carsales.com.au>
2018-12-07 17:17:49 +1100

    implement

--- app/src/main/java/au/com/kaiwu/nestfragmentdagger/main/customview/CustomView.kt
+++ app/src/main/java/au/com/kaiwu/nestfragmentdagger/main/customview/CustomView.kt
@@ -1,16 +1,27 @@
 package au.com.kaiwu.nestfragmentdagger.main.customview
 
 import android.content.Context
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.widget.FrameLayout
 import android.support.v4.content.ContextCompat.getSystemService
 import android.view.LayoutInflater
 import au.com.kaiwu.nestfragmentdagger.R
 import au.com.kaiwu.nestfragmentdagger.main.childfragment.ChildFragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
 import kotlinx.android.synthetic.main.custom_view.view.*
+import javax.inject.Inject
+import dagger.android.support.HasSupportFragmentInjector
 
 
-class CustomView(context: Context, fragmentManager: FragmentManager) : FrameLayout(context) {
+class CustomView(context: Context, fragmentManager: FragmentManager) : FrameLayout(context), HasSupportFragmentInjector {
+    @Inject
+    internal lateinit var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return dispatchingFragmentInjector
+    }
 
 
     init {
